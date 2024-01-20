05e20f2502d5d6a372edc89b21c40ff204d420b4
phiro98 <phiroze.pc14@gmail.com>
2024-01-20 11:51:15 +0530

    connection string edited

--- dkr/flask1_mongo/__init__.py
+++ dkr/flask1_mongo/__init__.py
@@ -1,12 +1,18 @@
 from flask import Flask
 from os import environ
 from flask_mongoengine import MongoEngine
+from pymongo import MongoClient
 
 app = Flask(__name__)
-app.config['MONGODB_SETTINGS'] = {
-    'host':'mongodb://{}:{}@mongo-nodeport-svc:32000/{}'.format(environ.get('username'),environ.get('password'),environ.get('db_name'))
-}
+# MongoDB configuration with user credentials
+mongodb_username = 'your_username'
+mongodb_password = 'your_password'
+mongodb_host = 'your_mongodb_host'  # Example: 'localhost' or 'mongodb.example.com'
+mongodb_port = 'your_mongodb_port'  # Example: 27017
+mongodb_database = 'your_database_name'
+mongodb_uri = f'mongodb://{mongodb_username}:{mongodb_password}@{mongodb_host}:{mongodb_port}/{mongodb_database}'
 app.config['SECRET_KEY']="ABC123"
-db = MongoEngine(app)
+db = MongoClient(mongodb_uri)
+
 
 from flask1_mongo import routes
